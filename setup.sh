8deabc767322e14c7368130e6eee5cdd4affcb0a
Taichi Sasaki <logphy@gmail.com>
2015-01-11 10:24:42 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 
 for dotfile in .?*
 do
-  if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+  if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitmodules' ] && [ $dotfile!= '.gitignore' ] && [ $dotfile != 'README.md' ]
   then
     ln -Fis "$PWD/$dotfile" $HOME
   fi
